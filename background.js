fe7e2da5d95365461a49bef00a9f7316264ad4f7
Kamil Pułka <kpulka247@gmail.com>
2025-04-29 15:24:19 +0200

    feat: Add new version notification

---
+++ background.js
@@ -0,0 +1,17 @@
+chrome.runtime.onInstalled.addListener((details) => {
+    if (details.reason === 'update') {
+      const version = chrome.runtime.getManifest().version;
+  
+      chrome.notifications.create({
+        type: 'basic',
+        iconUrl: 'icons/icon128.png',
+        title: 'Dark Connect',
+        message: `Updated to version ${version}. Click here to see what's new!`,
+        priority: 2
+      });
+    }
+  });
+  
+  chrome.notifications.onClicked.addListener(() => {
+    chrome.tabs.create({ url: 'https://chromewebstore.google.com/detail/dark-connect/nadhhgppikppmjacnkebagbgcibnfnob' });
+  });
