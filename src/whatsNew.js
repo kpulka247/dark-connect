d603b48e5857f6a60726b996e32bdae1bf816137
Kamil Pułka <kpulka247@gmail.com>
2026-04-13 23:37:31 +0200

    fix: Fix new followers panels and minor bugs

--- src/whatsNew.js
+++ src/whatsNew.js
@@ -1,6 +1,6 @@
 export const RELEASE_NOTES = [
+  "🐛 <b>Bug Fixes:</b> Fixed elements related to followers update, some inconsistencies and minor bugs.",
   "🌟 <b>Join Our Community!</b> We welcome contributors to help us fix bugs and build features. Found a bug or have an idea? Open an issue! If you want to contribute code, please read our CONTRIBUTING guidelines on GitHub to learn how to get started. ❤️",
-  "🐛 <b>Bug Fixes:</b> Fixed carousel card shadow, workout list icons, and challenge comment title colors.",
 ];
 
 export const CHANGELOG_URL =
